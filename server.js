f200be38876be6e57a5c1f0945e4ec8a13058eaa
Jasvir Singh Bargari <30661646+Jasvirbargari@users.noreply.github.com>
2017-08-04 19:29:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/article-one', function(req, res)
 app.get('/article-two', function(req, res)
 { res.send('Article two requested and will be served here')});
 
-app.get('/article-one', function(req, res)
+app.get('/article-three', function(req, res)
 { res.send('Article three requested and will be served here')});
 
 // Do not change port, otherwise your app won't run on IMAD servers
