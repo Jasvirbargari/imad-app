06e7510c78e31df742af3b30a17fe3cc6a8afb4e
Jasvir Singh Bargari <30661646+Jasvirbargari@users.noreply.github.com>
2017-08-16 21:19:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,17 +17,17 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/ui/article1', function(req, res) {
+app.get('article1', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
     
 });
 
-app.get('/ui/article2', function(req, res) { 
+app.get('article2', function(req, res) { 
     res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
     
 });
 
-app.get('/ui/article3', function(req, res) {
+app.get('article3', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 
