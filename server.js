69f83a9751c2594b7eccde248981c8772bbd813a
Jasvir Singh Bargari <30661646+Jasvirbargari@users.noreply.github.com>
2017-08-16 19:58:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('ui/', function (req, res) {
+app.get('/ui/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
